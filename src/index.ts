262d2a7aef990c7d765ae221682269a6beb7ca3b
antieatingactivist <antieatingactivist@gmail.com>
2023-03-21 14:16:26 -0700

    electron app up and running

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 780,
+    height: 580,
+    title: "Stella Invoice",
+    // fullscreen: true,
+    // kiosk: true,
+    // visualEffectState: "active",
+    // vibrancy: 'sidebar',
+    // resizable: false,
+    // maximizable: false,
+    // movable: false,
+    // titleBarStyle: "hidden",
+    // useContentSize: true,
+    // frame: false,
+    // show: false,
+    webPreferences: {
+      // nodeIntegration: false,
+      // contextIsolation: true,
+      // preload: path.join(__dirname, 'extensionScript.js')
+    }
+  });
+
+  // win.loadURL('https://platevoltage.github.io/stella-invoice/', {
+  // });
+  win.loadFile(path.join(__dirname, './build/index.html')) 
+
+  win.on('page-title-updated', function(e) {
+    e.preventDefault()
+  });
+  return win
+      
+};
+
+
+
+app.whenReady().then(async () => {
+
+  const win = createWindow();
+  win.webContents.executeJavaScript('console.log("test")')
+  // note: your contextMenu, Tooltip and Title code will go here!
+})
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
