7f59ffbfae0d9617bf34647590c07a2e2a812bcc
antieatingactivist <antieatingactivist@gmail.com>
2023-05-31 23:40:32 -0700

    fixed parsing

--- react/src/components/Options.tsx
+++ react/src/components/Options.tsx
@@ -11,10 +11,10 @@ export default function Options({invoiceItems, setInvoiceItems}: Props) {
     <div className="options">
         {Object.keys(invoiceItems).map( (key, i) => 
             <div key={i} style={{display: "flex", width: "50%"}}>
-              <input type="checkbox" id={key} name={key} value={key} checked={invoiceItems[key as keyof Columns]} onChange={() => {
-                  invoiceItems[key as keyof Columns] = !invoiceItems[key as keyof Columns];
-                  setInvoiceItems({...invoiceItems});
-              }}></input>
+                <input type="checkbox" id={key} name={key} value={key} checked={invoiceItems[key as keyof Columns]} onChange={() => {
+                    invoiceItems[key as keyof Columns] = !invoiceItems[key as keyof Columns];
+                    setInvoiceItems({...invoiceItems});
+                }}></input>
               <label htmlFor={key}>{columnDef[key as keyof Columns]}</label>
             </div>
         )}
