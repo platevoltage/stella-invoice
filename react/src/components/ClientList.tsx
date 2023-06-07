6a790dc055ccaac143a37eb63d3dfc5a2cff3cd7
antieatingactivist <antieatingactivist@gmail.com>
2023-06-07 15:58:51 -0700

    PDF now viewable in window

--- react/src/components/ClientList.tsx
+++ react/src/components/ClientList.tsx
@@ -1,7 +1,9 @@
 
+import { useState } from 'react';
 import { Columns, Tag, columnDef, Client } from '../interfaces';
 import './ClientList.css'
 import Papa from 'papaparse';
+import Invoice from './Invoice';
 
 interface Props {
     clientMetaData: Client[];
@@ -21,6 +23,7 @@ const unparseConfig: Papa.UnparseConfig = {
 }
 
 export default function ClientList({clientMetaData, inputCSV, invoiceItems}: Props) {
+  const [showInvoice, setShowInvoice] = useState(false);
   function processInvoices() {
     const csvArray = [];
     for (let client of clientMetaData) {
@@ -48,6 +51,10 @@ export default function ClientList({clientMetaData, inputCSV, invoiceItems}: Pro
     }
     window.localStorage.setItem("invoiceItems", JSON.stringify(invoiceItems));
     return csvArray;
+  }
+
+  function handleInvoice(_: any, index: number) {
+    setShowInvoice(true);
   }
 
   function handleDownload(_: any, index = -1) {
@@ -100,7 +107,7 @@ export default function ClientList({clientMetaData, inputCSV, invoiceItems}: Pro
               </svg>
             </button>
 
-            <button className="button" onClick={(e:any) => handleDownload(e, index)}>
+            <button className="button" onClick={(e:any) => handleInvoice(e, index)}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-receipt" viewBox="0 0 16 16">
                 <path d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z"/>
                 <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5zm8-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5z"/>
@@ -109,7 +116,9 @@ export default function ClientList({clientMetaData, inputCSV, invoiceItems}: Pro
             {client.name}
         </div>
         )}
-        
+        {
+          showInvoice && <Invoice />
+        }
     </div>
   )
 }
