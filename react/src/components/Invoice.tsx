88d7e3a035509374c1cbc4781d2cd7290cbc966e
antieatingactivist <antieatingactivist@gmail.com>
2023-06-14 01:29:54 -0700

    styled back button

--- react/src/components/Invoice.tsx
+++ react/src/components/Invoice.tsx
@@ -223,7 +223,7 @@ function Invoice({invoiceData, setShowInvoice}: Props) {
                 </div>
                 :
                 <>
-                    <button onClick={() => setShowInvoice(false)} style={{position: "fixed", left: 0, top: "80px", height: "40px", backgroundColor: "#323639", border: "none", borderRadius: "0 0 1em 0"}}>
+                    <button onClick={() => setShowInvoice(false)} style={{position: "fixed", left: 0, top: "87px", height: "34px", backgroundColor: "#323639", border: "none", borderRadius: "0 0 1em 0", boxShadow: "-0px -5px 0px #323639, 0px 0px 4px black"}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-left-fill" viewBox="0 0 16 16">
                             <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
                         </svg>
